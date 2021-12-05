94cd2de934aeaa4a579ee9007a4ad2d36b754bc5
Dmitriy Kukhta <dima.kukhta.2001@mail.ru>
2021-12-05 19:45:23 +0300

    fix: refactoring code

--- src/app.js
+++ src/app.js
@@ -1,7 +1,3 @@
-// const express = require('express');
-// const swaggerUI = require('swagger-ui-express');
-// const path = require('path');
-// const YAML = require('yamljs');
 const fastify = require('fastify');
 const userRouter = require('./resources/users/user.router');
 const boardRouter = require('./resources/boards/board.router');
@@ -12,9 +8,5 @@ const app = fastify({ logger: true });
 app.register(userRouter);
 app.register(boardRouter);
 app.register(taskRouter);
-
-// const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
-
-// app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 module.exports = app;
