b29cfbfb73c7b7b31a74990324121de861e5048e
Dmitriy Kukhta <dima.kukhta.2001@mail.ru>
2022-01-22 17:00:00 +0300

    refactor: function

--- src/resources/tasks/task.memory.repository.ts
+++ src/resources/tasks/task.memory.repository.ts
@@ -7,7 +7,7 @@ import OrmTask from './task.model';
  * Should return array of tasks where id of board === boardId
  * @returns array of tasks or emty array if tasks not exists
  */
- export const getAll = async () => {
+ const getAll = async () => {
   const allTasks = await getRepository(OrmTask).find();
   return allTasks;
 }
@@ -17,7 +17,7 @@ import OrmTask from './task.model';
  * @param taskId 
  * @returns task  of false if task not exist
  */
- export const getTask = async (taskId: ITask['id']) => {
+ const getTask = async (taskId: ITask['id']) => {
   const task = await getRepository(OrmTask).findOne(taskId);
   return task;
 }
@@ -26,7 +26,7 @@ import OrmTask from './task.model';
  * @param task - data for new task
  * @returns new task
  */
- export const createTask = async (task: ITask) => {
+ const createTask = async (task: ITask) => {
   const newTask = await getRepository(OrmTask).insert(task);
   return newTask;
 };
@@ -37,7 +37,7 @@ import OrmTask from './task.model';
  * @param task - new data for task
  * @returns updated task or null if task not exist
  */
- export const updateTask = async (id: ITask['id'], task: ITask) => {
+ const updateTask = async (id: ITask['id'], task: ITask) => {
   const updatedTask = await getRepository(OrmTask).update(id, task);
   return updatedTask;
 };
@@ -47,7 +47,7 @@ import OrmTask from './task.model';
  * @param id - task Id
  * @returns true - if task has been updated or false otherwise
  */
- export const deleteTask = async (id: ITask['id']) => {
+ const deleteTask = async (id: ITask['id']) => {
   const task = await getRepository(OrmTask).delete(id);
   return task;
 };
@@ -56,7 +56,7 @@ import OrmTask from './task.model';
  * Should delete tasks that belonged to a remote board
  * @param boardId - id of board
  */
- export const deleteBoardTasks = async (boardId: ITask['boardId']) => {
+ const deleteBoardTasks = async (boardId: ITask['boardId']) => {
   await getRepository(OrmTask).delete({ boardId });
  }
 
@@ -64,7 +64,7 @@ import OrmTask from './task.model';
  * Should updated tasks where task.userId === userId, and set null value in the field
  * @param userId
  */
- export const updateDeleteUserTasks = async (userId: IUser['id']) => {
+ const updateDeleteUserTasks = async (userId: IUser['id']) => {
   const updatedTasks = await getRepository(OrmTask).update({ userId }, { userId: undefined });
   return updatedTasks;
  }
