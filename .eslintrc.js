f2e318b210d8faba47f493c91c311caa76c78fe2
Dmitriy Kukhta <dima.kukhta.2001@mail.ru>
2022-01-30 17:16:35 +0300

    fix: eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,8 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: ['tsconfig.json'],
     sourceType: 'module',
-    ecmaVersion: 2021,
-    ecmaFeatures: {
-      impliedStrict: true,
-    },
-    requireConfigFile: false,
-  },
-  settings: {
-    noInlineConfig: true,
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.cjs'],
-      },
-    },
-    node: {
-      allowModules: ['electron'],
-      resolvePaths: [__dirname],
-      tryExtensions: ['.js', '.json', '.node', '.ts', '.cjs'],
-    },
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
@@ -31,13 +13,12 @@ module.exports = {
   env: {
     node: true,
     jest: true,
-    es2020: true,
-    jasmine: true,
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 };
